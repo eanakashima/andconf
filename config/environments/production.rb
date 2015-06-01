e81b54469fec15c352d74efbfc5861bbd3565488
Lillie Chilen <lillie.chilen@gmail.com>
2015-05-31 17:27:36 -0700

    Add donations page and Stripe integration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,4 +75,13 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.smtp_settings = {
+    address:   "smtp.mandrillapp.com",
+    port:      25,
+    user_name: ENV["MANDRILL_USERNAME"],
+    password:  ENV["MANDRILL_API_KEY"]
+  }
+
+  config.action_mailer.default_url_options = { host: ENV['HOST_URL'] }
 end
