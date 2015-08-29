1ab5fef696b5c6715930dfafa71013d471acd606
Emily Nakashima <emily@bugsnag.com>
2015-08-28 21:19:41 -0700

    Get rid of donations/sponsorships
    So we can decommission our SSL endpoint for a while

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   get 'code_of_conduct' => 'static_pages#code_of_conduct'
   get 'details' => 'static_pages#details'
 
-  resource :donations, only: [:show, :create]
+  # resource :donations, only: [:show, :create]
 
-  get '/scholarships', to: redirect("https://docs.google.com/forms/d/1UUp6415niIzBoMxmhk6mxRfmgCfO-1ioc8oEZaNCt4o")
-  get '/apply', to: redirect("http://goo.gl/forms/vJcWWyRykr")
+  # get '/scholarships', to: redirect("https://docs.google.com/forms/d/1UUp6415niIzBoMxmhk6mxRfmgCfO-1ioc8oEZaNCt4o")
+  # get '/apply', to: redirect("http://goo.gl/forms/vJcWWyRykr")
 end
